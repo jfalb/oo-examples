55de876a2a83547036e8648541c80fe9d9ac5fef
Juergen Falb <j.falb@nousdigital.net>
2021-12-07 10:46:42 +0100

    Added comment

--- src/main/java/at/ac/fhcampuswien/subtyping/BaseTypeA.java
+++ src/main/java/at/ac/fhcampuswien/subtyping/BaseTypeA.java
@@ -2,6 +2,7 @@ package at.ac.fhcampuswien.subtyping;
 
 public class BaseTypeA {
 
+    /** method1 example for base class */
     public void method1() {
         System.out.println("Base method");
     }
